939d03d3cfd03d7d08360ee7244a24bac079653c
AlirezaSadeghiPour <alirezasp404@gmail.com>
2024-05-15 06:50:55 +0330

    feat: create EventRepository

---
+++ WhenWhere.Core/Domain/RepositoryContracts/IEventsRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+using System;
+using WhenWhere.Core.Domain.Entities;
+using WhenWhere.Core.DTO;
+
+namespace WhenWhere.Core.Domain.RepositoryContracts
+{
+    public interface IEventsRepository
+    {
+        /// <summary>
+        /// Create an Event object in data store.
+        /// </summary>
+        /// <param name="eventModel">Event object to create.</param>
+        Task CreateEvent(Event eventModel);
+
+        /// <summary>
+        /// Returns all Events in the data store.
+        /// </summary>
+        /// <param name="userId">UserId.</param>
+        /// <returns>List of Events objects from table. </returns>
+        IQueryable<Event>? GetAllEvents(Guid? userId);
+
+        /// <summary>
+        /// Returns all created Events by given userId.
+        /// </summary>
+        /// <param name="userId">UserId.</param>
+        /// <returns>all created Events by given userId. </returns>
+        IQueryable<Event>? GetCreatedEvents(Guid? userId);
+
+        /// <summary>
+        /// Returns all registered Events by given userId.
+        /// </summary>
+        /// <param name="userId">UserId.</param>
+        /// <returns>all created registered by given userId. </returns>
+        IQueryable<Event?>? GetRegisteredEvents(Guid? userId);
+
+        /// <summary>
+        /// register an user in selected event.
+        /// </summary>
+        /// <param name="userId">user Id to register.</param>
+        /// <param name="eventId">event Id to register.</param>
+        Task RegisterForEvent(RegisteredEvent registeredEvent);
+    }
+}
---
+++ WhenWhere.Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WhenWhere.Core.Domain.Entities;
+using WhenWhere.Core.Domain.RepositoryContracts;
+using WhenWhere.Infrastructure.DataContext;
+
+namespace WhenWhere.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Event repository for access database.
+    /// </summary>
+    public class EventRepository : IEventsRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EventRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CreateEvent(Event eventModel)
+        {
+            _dbContext.Add(eventModel);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<Event>? GetAllEvents(Guid? userId)
+        {
+           return _dbContext.Events.Include(nameof(Event))
+                                   .Where(e => e.EventCreatorId != userId);
+
+        }
+
+        public IQueryable<Event> GetCreatedEvents(Guid? userId)
+        {
+            return _dbContext.Events.Where(e => e.EventCreatorId == userId);
+        }
+
+        public IQueryable<Event?>? GetRegisteredEvents(Guid? userId)
+        {
+            return _dbContext.RegisteredEvents?.Include(nameof(Event))
+                                              .Where(e => e.UserId == userId)
+                                              .Select(e => e.Event);
+        }
+
+        public async Task RegisterForEvent(RegisteredEvent registeredEvent)
+        {
+            _dbContext.Add(registeredEvent);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
