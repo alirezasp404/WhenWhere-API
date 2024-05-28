ac139c86dc1b30644876666d0c6f8abc8dfe1f6b
AlirezaSadeghiPour <alirezasp404@gmail.com>
2024-05-28 16:34:27 +0330

    feat :create RegisterEventcontroller

--- WhenWhere.API/Controllers/EventsController.cs
+++ WhenWhere.API/Controllers/EventsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using WhenWhere.Core.Domain.Entities;
 using WhenWhere.Core.DTO;
 using WhenWhere.Core.Enums;
 using WhenWhere.Core.ServiceContracts;
@@ -35,7 +36,7 @@ namespace WhenWhere.API.Controllers
         {
             var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var allEvents = await _eventsGetterService.GetAllEvents(userId);
-            if (!allEvents.Any())
+            if (allEvents is null || !allEvents.Any())
             {
                 return Problem(statusCode: StatusCodes.Status404NotFound, detail: "There aren't any Events");
             }
@@ -48,7 +49,7 @@ namespace WhenWhere.API.Controllers
         {
             var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var createdEvents = await _eventsGetterService.GetCreatedEvents(userId);
-            if (!createdEvents.Any())
+            if (createdEvents is null || !createdEvents.Any())
             {
                 return Problem(statusCode: StatusCodes.Status404NotFound, detail: "There aren't any Created Events");
             }
---
+++ WhenWhere.API/Controllers/RegisterEventsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WhenWhere.Core.DTO;
+using WhenWhere.Core.Exceptions;
+using WhenWhere.Core.ServiceContracts;
+using WhenWhere.Core.Services;
+
+namespace WhenWhere.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterEventsController : ControllerBase
+    {
+        private readonly IEventsGetterService _eventsGetterService;
+        private readonly IRegisterEventsService _registerEventsService;
+
+        public RegisterEventsController(IEventsGetterService eventsGetterService,IRegisterEventsService registerEventsService)
+        {
+            this._eventsGetterService = eventsGetterService;
+            this._registerEventsService = registerEventsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EventResponse>>> GetRegisteredEvents()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var registeredEvents = await _eventsGetterService.GetRegisteredEvents(userId);
+            if (registeredEvents is null || !registeredEvents.Any())
+            {
+                return Problem(statusCode: StatusCodes.Status404NotFound, detail: "There aren't any Registered Events");
+            }
+
+            return Ok(registeredEvents);
+        }
+        [HttpPost]
+        public async Task<ActionResult<EventResponse>> RegisterEvent( [FromBody] Guid? eventId)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            try
+            {
+            await _registerEventsService.RegisterEvent(eventId, userId);
+
+            }catch (ArgumentNullException ex) {
+                return Problem(detail: "There aren't any events with given Id", statusCode: StatusCodes.Status404NotFound);
+            }catch (ArgumentException ex ) {
+                return Problem(detail: ex.Message, statusCode: StatusCodes.Status400BadRequest);
+            }
+            return NoContent();
+        }
+    }
+}
